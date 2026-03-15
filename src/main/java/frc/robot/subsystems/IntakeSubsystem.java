07ec4a74bbc9d6f64c7853b072a62fb2cf610647
Adam <adamlafeer@gmail.com>
2026-03-15 10:24:58 -0400

    Temparilly removed the follow motor in the intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -14,22 +14,22 @@ import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase implements ExtendedSubsystem, TogglableSubsystem {
 	private SparkMax leadMotor;
-	private SparkMax followMotor;
+	// private SparkMax followMotor;
 
 	private double currentSpeed;
 	private boolean isActive;
 
 	public IntakeSubsystem() {
 		leadMotor = new SparkMax(IntakeConstants.LEAD_MOTOR_CAN_ID, MotorType.kBrushless);
-		followMotor = new SparkMax(IntakeConstants.FOLLOW_MOTOR_CAN_ID, MotorType.kBrushless);
+		// followMotor = new SparkMax(IntakeConstants.FOLLOW_MOTOR_CAN_ID, MotorType.kBrushless);
 
 		SparkMaxConfig leadMotorConfig = new SparkMaxConfig();
 		leadMotorConfig.idleMode(IdleMode.kBrake);
 		leadMotor.configure(leadMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
-		SparkMaxConfig followMotorConfig = new SparkMaxConfig();
-		followMotorConfig.idleMode(IdleMode.kBrake);
-		followMotor.configure(followMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+		// SparkMaxConfig followMotorConfig = new SparkMaxConfig();
+		// followMotorConfig.idleMode(IdleMode.kBrake);
+		// followMotor.configure(followMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
 		currentSpeed = 0;
 		isActive = false;
@@ -43,7 +43,7 @@ public class IntakeSubsystem extends SubsystemBase implements ExtendedSubsystem,
 	@Override
 	public void start(double speed) {
 		leadMotor.set(speed);
-        followMotor.set(speed * IntakeConstants.SPEED_RATIO);
+        // followMotor.set(speed * IntakeConstants.SPEED_RATIO);
 		currentSpeed = speed;
 		isActive = true;
 	}
@@ -51,7 +51,7 @@ public class IntakeSubsystem extends SubsystemBase implements ExtendedSubsystem,
 	@Override
 	public void stop() {
 		leadMotor.set(0);
-        followMotor.set(0);
+        // followMotor.set(0);
 		currentSpeed = 0;
 		isActive = false;
 	}
