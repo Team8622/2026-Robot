e4c6a88407dae83088053dc7dbf179588c55fa0c
Aidam142 <adamlafeer@gmail.com>
2026-03-23 17:33:55 -0400

    Changed Intake CAN ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
 	public static class IntakeConstants {
 		// public static final int LEAD_MOTOR_CAN_ID = 13;
 		// public static final int FOLLOW_MOTOR_CAN_ID = 14;
-		public static final int LEAD_MOTOR_CAN_ID = 14;
+		public static final int LEAD_MOTOR_CAN_ID = 13;
 
 		public static final double FORWARD_SPEED = -0.5;
 		public static final double BACKWARD_SPEED = 0.5;
