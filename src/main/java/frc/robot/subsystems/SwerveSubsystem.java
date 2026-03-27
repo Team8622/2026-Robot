c73b2ec5173a49176eb5d1e9397ac066fde0e4bc
Aidam142 <adamlafeer@gmail.com>
2026-03-27 19:50:06 -0400

    Removed switching hub poses for pathfinding command, as the pathfinding was already alliance-relative

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -243,10 +243,7 @@ public class SwerveSubsystem extends SubsystemBase {
 
     private Rotation2d getHubYaw() {
         // Taken from PhotonUtils.getYawToPose()
-        Pose3d hubAprilTagPose = DriverStation.getAlliance().get() == Alliance.Blue
-                ? TargetConstants.BLUE_HUB_POSE3D
-                : TargetConstants.RED_HUB_POSE3D;
-        Translation2d relativeTrl = hubAprilTagPose.toPose2d().relativeTo(getPose()).getTranslation();
+        Translation2d relativeTrl = TargetConstants.HUB_POSE3D.toPose2d().relativeTo(getPose()).getTranslation();
         return new Rotation2d(relativeTrl.getX(), relativeTrl.getY()).plus(swerveDrive.getOdometryHeading());
     }
 
